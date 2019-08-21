05383f066634c4188bb05c7cbbc3c303d05f9f35
Yashira Baggett <Yashira@Yashiras-MacBook-Air.local>
2019-08-21 11:52:54 -0700

    loops and fizzbuzz review complete

--- code-alongs/arrays-loops-functions/app.js
+++ code-alongs/arrays-loops-functions/app.js
@@ -57,10 +57,120 @@ while(numberToTen < 11){
     numberToTen++;
 }
 
-//while loop that counts down from ten
+//while loop that counts down from ten:
 
 let numberFromTen = 10;
 while(numberFromTen >= 0){
     console.log(`The number is ${numberFromTen}`)
     numberFromTen--;
 }
+
+//for loops---for loops bundles iterator initialization (always i), then the condition (after each iteration it will check that condition again and based on true or false we'll see a different result), and the the progress statement---an incrementer
+//the benefit is that it's self-contained--so i wont hang around after the loop is complete. No variable hanging around. 
+for(let i = 0; i < 11; i ++){
+    console.log(`i is ${i}`);
+}
+
+//if we were to change the incrementer to i --, we would create an infinite loop
+
+
+//for loop that counts from 99 to 999:
+
+for(let i = 99; i < 1000; i ++){
+    console.log(`i is ${i}`)
+}
+
+//for loop that counts from 99 to 999 by twos:
+
+for(let i = 99; i < 1000; i +=2){
+    console.log(`i is ${i}`)
+}
+
+//for loop that counts down from 99 to 999 by twos:
+
+for(let i = 1000; i > 98; i -= 2){
+    console.log(`i is ${i}`)
+}
+
+//When do I use a while loop and when do I use a for?
+
+//For loops are the choice when you know exactly how long the code should run
+//While loops are great when you don't know how long it's going to take or when you are done
+
+//Example: We want this diceroll to end up bigger than .75
+
+// let diceRoll = 0;
+// while(diceRoll < .75){
+//  //we will not console log, instead we will assign diceRoll a method that will give a random number between 0 and .999999999. We DON'T KNOW how long this is going to take---what if the first 10,000 results of math.random are all under .75?
+//  //so we have to use a while loop here because we know that when we're done (when we have a roll that we want to keep)
+//  diceRoll = Math.random();
+// }
+// //be careful, the above loop could run forever!
+
+
+
+//You can combine for loops and if checks as well:
+//if we only want to count the odd numbers inside of this loop, we can put an if-check inside
+for(let i = 0; i < 11; i ++){
+ //when I divide i by two (use modulus), if the remainder is one then console.log that number 
+        if(i % 2 == 1){
+        console.log(`i is ${i}`);
+    }
+   
+}
+
+//A cleaner version would cut out the if-check and use i += 2
+//you just start i at 1 and have it go up by 2. It will cut the amount of time you run this code in half.
+
+for(let i = 1; i < 11; i +=2){
+           console.log(`i is ${i}`);
+ }
+      
+//Code Challenge: FizzBuzz
+
+//loop up to 30. For each number if the number is divisible by 3, log "fizz"
+//if the number is divisible by 5, log "buzz"
+//if the number is divisible by BOTH, log "fizzbuzz"
+//otherwise, log the number
+//each number should only log one message
+
+
+for(let i = 0; i < 31; i ++){
+    if(i % 3 === 0){
+        console.log(i + " fizz")
+    if (i % 5 === 0){
+        console.log(i + "buzz")
+        };
+    if(i % 15 === 0){
+        console.log( i + "fizzbuzz")}else{
+            console.log(i)
+        }
+    }
+}
+     //we need an else if because we only want one thing to happen 
+    // so far its logging both the number and the quote
+
+
+for(let i = 0; i < 31; i ++){
+    if(i % 15 === 0){
+        console.log("fizzbuzz")
+    }else if (i % 3 === 0){
+        console.log("fizz")
+    }else if(i % 5 === 0){
+        console.log("buzz")
+        }else{
+            console.log(i)
+        }
+    }
+
+    
+    //you can also do it this way (modern js shortcut)
+
+    for(let i = 0; i < 31; i ++)
+        if(i % 15 === 0) console.log("fizzbuzz")
+        else if (i % 3 === 0) console.log("fizz")
+        else if(i % 5 === 0)console.log("buzz")
+        else{console.log(i)
+            }
+    //(as long as the code block you are trying to execute is one tiny line you can get away with it)      
+    
