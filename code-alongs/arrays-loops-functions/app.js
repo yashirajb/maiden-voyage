a6712db4928b5d2959c8483cce6261c0610660ca
Yashira Baggett <Yashira@Yashiras-MacBook-Air.local>
2019-08-21 09:19:17 -0700

    while loop practice complete

--- code-alongs/arrays-loops-functions/app.js
+++ code-alongs/arrays-loops-functions/app.js
@@ -19,3 +19,48 @@ if(theBestClass === "Starfleet"){
 
 //While Loops
 //While loops are alot like if-checks. but the if-check says do this once while the WHILE loop says keep doing the thing until the condition is false, so you have to be really careful with while loops because they can become infinite
+
+//We have a number and we want to count up to 10
+//Non-loop version:
+
+let number = 1; 
+console.log("The number is " + number);
+//add 1 to it
+number += 1;
+//or number ++ or number = number + 1
+console.log("The number is " + number)
+//now console.log that over and over
+number += 1;
+console.log("The number is " + number)
+number += 1;
+console.log("The number is " + number)
+number += 1;
+console.log("The number is " + number)
+number += 1;
+console.log("The number is " + number)
+number += 1;
+console.log("The number is " + number)
+number += 1;
+console.log("The number is " + number)
+
+//We are repeating ourselves too muc----not DRY at all
+//Instead we can put this operation inside a loop and tell it when to stop
+
+let numberToTen = 1;
+while(numberToTen < 11){
+    //using string interpolation
+    console.log(`The number is ${numberToTen}`);
+    //if we ran this right now it would never end because it would execute the code block and go back into the condition again over and over and over again
+    //so you must make sure you are doing something inside the code block that will help you reach that condition
+    // add number ++ (this is the something that will help us reach the condition)
+    //also changed 10 on line 50 to 11---you'll see this way more than number <= 10, the former is the convention
+    numberToTen++;
+}
+
+//while loop that counts down from ten
+
+let numberFromTen = 10;
+while(numberFromTen >= 0){
+    console.log(`The number is ${numberFromTen}`)
+    numberFromTen--;
+}
