ef4bd14a730c65e023cbfb375ed33bf60f78cd9e
Yashira Baggett <Yashira@Yashiras-MacBook-Air.local>
2019-08-22 09:07:01 -0700

    reviewed objects vs arrays, how to access nested data, invoking methods, creating and accessing object

--- code-alongs/objects/app.js
+++ code-alongs/objects/app.js
@@ -1,29 +1,58 @@
-//write this:
-//the function called findMax takes in an array
-//console log right away to see if it's working
+//Objects
 
-function findMax(arr){
-    console.log(arr)
-    //anything inside of a function we need to hold on to requires a variable
-    let max = 0;
-    //then a for loop to access each number
-    for(let i = 0; i < arr.length; i++)
-    //i is the address
-    //arr[i] is what LIVES at the address
-    console.log(arr[i])
-    //1. Compare the numbers being said and throw out the smallest of the two (holding on to the larger number each time)
-    if(arr[i] > max){
-        // console.log(`${arr[i]} is bigger than ${max}`)
-        max = arr[i];
+//Unlike arrays, objects allow us to store information without having to remember exactly what spot they are in 
+const meArray = ["Yashira", "Baggett", "Goldsboro"]
+//to represent real life objects that make sense to all involved. The above array wouldn't really make sense to someone who doesn't know me
+
+
+const meMakesMoreSenseArray = {
+    "key" : "value",
+    "firstname": "Yashira",
+    "lastname": "Baggett",
+    "hometown": "Goldsboro"
+}
+
+//How do I get things out?
+//Use the name of the object + either dot or bracket notation
+
+console.log(meMakesMoreSenseArray.firstname)
+//"Yashira"
+
+//What about nested data?
+
+const meMakesEvenMoreSenseArray = {
+    "key" : "value",
+    "firstname": "Yashira",
+    "lastname": "Baggett",
+    "livedTowns": [{"place":"Goldsboro"},{"place":"Japan"},{"place":"San Diego"},{"place":"Riverside"},{"place":"Los Angeles"}],
+    "teach": function(reading){
+        console.log(`Today we will practice ${reading}`)
     }
 }
 
-return max;
-//execute it ---provide name of function and pass in any params and call it
-const arrayNums = [1,2,3,4,5]
-const biggestNum = findMax(arrayNums)
-console.log(biggestNum)
+//What if I wanted to grab Los Angeles from the nested array?
 
-//how would you find the biggest number?
-//1. Compare the numbers being said and throw out the smallest of the two (holding on to the larger number each time)
+console.log(meMakesEvenMoreSenseArray.livedTowns[4].place)
+//Result is Los Angeles
 
+//Objects can store functions, and these functions are called METHODS
+//you can INVOKE THE METHOD by using the name of the object and the name of the method and you can pass in anyy parameter you desire 
+
+meMakesEvenMoreSenseArray.teach("how to break syllables correctly")
+//Result is Today we will practice how to break syllables correctly
+
+//Note: Key names don't have to be in quotes but may be useful when code gets more complicated
+
+//Create a BigFoot object with shoe size, weight, and current location, as well as height, hair color, and demeanor
+
+const bigFoot = {
+    "height": "10 feet",
+    "weight": "568 pounds",
+    "shoeSize": "15",
+    "currentLocation": "undetermined",
+    "hairColor": "ashen-blonde",
+    "demeanor": "placid"
+}
+console.log(bigFoot.weight)
+console.log(bigFoot.hairColor)
+console.log(bigFoot.demeanor)
