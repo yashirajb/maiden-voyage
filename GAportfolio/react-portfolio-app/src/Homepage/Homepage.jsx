ddad7b92076438221761e6484039ce049cba0a51
Yashira Baggett <Yashira@Yashiras-MacBook-Air.local>
2019-11-08 12:51:36 -0800

    changes to homepage

--- GAportfolio/react-portfolio-app/src/Homepage/Homepage.jsx
+++ GAportfolio/react-portfolio-app/src/Homepage/Homepage.jsx
@@ -5,30 +5,57 @@ import { Jumbotron, Container } from 'reactstrap';
 function Homepage() {
     return (
       <div className="App">
-               <div>
+          <div>
+                 
+           <Jumbotron fluid>
+          <Container fluid>
+            {/* <h1 className="display-3">Welcome</h1>
+            <p className="lead">Nice to see you, please take a look around.</p> */}
+          </Container>
+          <br/>
+          <br/>
+          
+        </Jumbotron>
+        <Jumbotron fluid>
+          {/* <Container fluid>
+            <h1 className="display-3">Welcome</h1>
+            <p className="lead">Nice to see you, please take a look around.</p>
+          </Container> */}
+        </Jumbotron>
+        <Jumbotron fluid>
+          {/* <Container fluid>
+            <h1 className="display-3">Welcome</h1>
+            <p className="lead">Nice to see you, please take a look around.</p>
+          </Container> */}
+        </Jumbotron>
         <Jumbotron fluid>
           <Container fluid>
             <h1 className="display-3">Welcome</h1>
             <p className="lead">Nice to see you, please take a look around.</p>
           </Container>
         </Jumbotron>
+        <Jumbotron fluid>
+          {/* <Container fluid>
+            <h1 className="display-3">Welcome</h1>
+            <p className="lead">Nice to see you, please take a look around.</p>
+          </Container> */}
+        </Jumbotron>
+        <Jumbotron fluid>
+          <Container fluid>
+            {/* <h1 className="display-3">Welcome</h1>
+            <p className="lead">Nice to see you, please take a look around.</p> */}
+          </Container>
+        </Jumbotron>
+        <Jumbotron fluid>
+          {/* <Container fluid>
+            <h1 className="display-3">Welcome</h1>
+            <p className="lead">Nice to see you, please take a look around.</p>
+          </Container> */}
+        </Jumbotron>
         <br />
-        <br />
-        <br />
-        <div className="resumeplaceholder">
-           {/* <ProjectPlaceHolder /> */}
-          <br/>
-          <br/>
-          <br/>
-           {/* <ProjectPlaceHolder /> */}
-           <br/> 
-           <br/>
-           <br/>
-           {/* <ProjectPlaceHolder /> */}
-           <br/>
-           <br/>
-           <br/>
      
+        <div className="resumeplaceholder">
+          
       </div>
       </div>
       </div>
